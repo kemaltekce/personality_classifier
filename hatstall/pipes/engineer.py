89b17795ecd6f3ec815cd83d4c680db91bc441d5
Kemal Tekce <k.tekce@foodtracks.de>
2020-06-01 19:25:51 +0200

    added debugger and average word calc pipes

--- hatstall/pipes/engineer.py
+++ hatstall/pipes/engineer.py
@@ -1,10 +1,31 @@
+import numpy as np
+
 from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class Debugger(BaseEstimator, TransformerMixin):
+
+    def transform(self, X, *_):
+        import ipdb
+        ipdb.set_trace()
+
+    def fit(self, *_):
+        return self
 
 
 class PostsJoiner(BaseEstimator, TransformerMixin):
 
     def transform(self, X, *_):
         return [' '.join(x) for x in X]
+
+    def fit(self, *_):
+        return self
+
+
+class AverageWordCalculator(BaseEstimator, TransformerMixin):
+
+    def transform(self, X, *_):
+        return [[np.mean([len(x.split()) for x in X_])] for X_ in X]
 
     def fit(self, *_):
         return self
