612fc53eff63ef9c9c0bcf69aa116084e26fb493
Kemal Tekce <k.tekce@foodtracks.de>
2020-06-03 20:32:42 +0200

    added token checker in test data

--- hatstall/pipes/model.py
+++ hatstall/pipes/model.py
@@ -1,3 +1,6 @@
+import numpy as np
+import re
+
 from sklearn.metrics import confusion_matrix, f1_score
 
 from hatstall.pipes import Pipe
@@ -23,3 +26,35 @@ class Evaluator(Pipe):
                 Personality.SECOND_PERSONALITY
             ])
         print(cm)
+
+
+class TestDataTokenChecker(Pipe):
+
+    def run(self):
+        # get feature names of vectorizer/tokenizer
+        model = self.payload['model']
+        feature_pipeline = model.named_steps['features']
+        token_pipeline = [
+            x for x in feature_pipeline.transformer_list
+            if x[0] == 'token'][0][1]
+        tokens = token_pipeline.named_steps['vect'].get_feature_names()
+
+        # get token pattern
+        pattern = token_pipeline.named_steps['vect'].token_pattern
+
+        # only keep data for which models knows more than 70% of the words
+        train_x, train_y, test_x, test_y = self.payload['train_test']
+        test_x_ = [' '.join(x) for x in test_x]
+        test_tokens_x = [re.findall(pattern, x) for x in test_x_]
+        missing = np.array([
+            len((set(x) - set(tokens))) / len(set(x)) for x in test_tokens_x])
+        keep = missing < 0.3
+        print(
+            f"Removing {len(test_x) - sum(keep)} out of {len(test_x)} test "
+            "examples because model knows to few words in test example.")
+
+        test_x = np.array(test_x)
+        test_x = list(test_x[keep])
+        test_y = np.array(test_y)
+        test_y = list(test_y[keep])
+        self.payload['train_test'] = train_x, train_y, test_x, test_y
